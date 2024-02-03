ff557cd80f185b4008bc13b5cfd2e5bfe10413a2
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-03 16:51:01 +0100

    third point detection enhancement and laser point extraction

--- src/laserPlane.py
+++ src/laserPlane.py
@@ -10,8 +10,8 @@ def find_laser_plate_point(frame, center):
     cropped = cv.cvtColor(cropped, cv.COLOR_BGR2HSV)
     # cropped = cv.medianBlur(cropped, 5)
 
-    mask1 = cv.inRange(cropped, (0, 60, 230), (20, 255, 255))
-    mask2 = cv.inRange(cropped, (150, 60, 230), (180, 255, 255))
+    mask1 = cv.inRange(cropped, (0, 55, 230), (20, 255, 255))
+    mask2 = cv.inRange(cropped, (150, 55, 230), (180, 255, 255))
     cropped = mask1 | mask2
 
     cv.imshow('test', cropped)
@@ -29,23 +29,64 @@ def find_laser_plate_point(frame, center):
 
 
 def detect_laser_points(frame, ellipse):
+    test = cv.medianBlur(frame, 3)
+    test = cv.cvtColor(test[
+                       round(ellipse[0][1] - ellipse[1][0] / 2): round(ellipse[0][1] + ellipse[1][0] / 2),
+                       round(ellipse[0][0] - ellipse[1][1] / 2): round(ellipse[0][0] + ellipse[1][1] / 2),
+                       :], cv.COLOR_BGR2HSV)
     cv.ellipse(frame, ellipse, (0, 255, 0), thickness=5)
     redChannel = frame[
                  round(ellipse[0][1] - ellipse[1][0] / 2): round(ellipse[0][1] + ellipse[1][0] / 2),
                  round(ellipse[0][0] - ellipse[1][1] / 2): round(ellipse[0][0] + ellipse[1][1] / 2),
                  2]
-    redChannel = cv.medianBlur(redChannel, 5)
-    _, laser = cv.threshold(redChannel, 235, 255, cv.THRESH_BINARY)
-    laser = cv.morphologyEx(laser, cv.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+    hej = test.copy()
+
+    redChannel = cv.medianBlur(redChannel, 3)
+
+    # _, laser = cv.threshold(redChannel, 255, 255, cv.THRESH_BINARY)
+    # laser = cv.morphologyEx(laser, cv.MORPH_DILATE, np.ones((5, 5), np.uint8))
     # laser = cv.morphologyEx(laser, cv.MORPH_OPEN, np.ones((5, 5), np.uint8))
+
+    mask1 = cv.inRange(test, (0, 70, 230), (10, 255, 255))
+    mask2 = cv.inRange(test, (160, 70, 230), (180, 255, 255))
+
+    test = mask1 | mask2
+    # test = cv.morphologyEx(test, cv.MORPH_ERODE, np.ones((5, 5), np.uint8))
+
     intersections = []
-    for i in range(0, len(laser)):
-        for j in range(0, len(laser[i])):
-            if laser[i][j] > 0:
+    '''
+    for i in range(0, len(test)):
+        flag = False
+        for j in range(0, len(test[i])):
+            if test[i][j] > 0 and not flag:
                 intersections.append(
                     [j + round(ellipse[0][0] - ellipse[1][1] / 2), i + round(ellipse[0][1] - ellipse[1][0] / 2)])
+                flag = True
                 break
+            elif test[i][j] > 0:
+                pass
+                # laser[i][j] = 0
+    '''
 
-    frame = laser
-    cv.imshow("intersections", frame)
+    height, width = test.shape
+
+    for y in range(height):
+        flag = False
+        for x in range(width):
+            if test[y][x] > 0 and not flag:
+                intersections.append(
+                    [x + round(ellipse[0][0] - ellipse[1][1] / 2), y + round(ellipse[0][1] - ellipse[1][0] / 2)])
+                flag = True
+                hej[y][x] = [50, 255, 255]
+                break
+            elif test[y][x] > 0:
+                pass
+                # laser[i][j] = 0
+
+    # frame = laser
+    # cv.imshow("intersections", laser)
+    cv.imshow("plate laser", hej)
+    cv.imshow("red", test)
+
     return intersections

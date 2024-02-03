ae57c561f284e0ce21b88382ee232999e86f2f48
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-03 18:31:23 +0100

    global error handling

--- src/scanner.py
+++ src/scanner.py
@@ -61,86 +61,91 @@ while cap.isOpened():
 
     original = frame.copy()
 
-    processedFrame = pre_process_frame(frame, debug=debug)
-    contours, hierarchy = cv.findContours(processedFrame, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    try:
+        processedFrame = pre_process_frame(frame, debug=debug)
+        contours, hierarchy = cv.findContours(processedFrame, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
-    rectangle = find_rectangle(contours)
-    back_r, back_t, back_a, back_b, back_c = process_rectangle(frame, rectangle, mtx, dist,
-                                                               debug=debug)
+        rectangle = find_rectangle(contours)
+        back_r, back_t, back_a, back_b, back_c = process_rectangle(frame, rectangle, mtx, dist,
+                                                                   debug=debug)
 
-    plate_elements = find_plate_elements(frame, contours, w, h)
-    plate, plate_r, plate_t = process_plate(plate_elements, frame, original, mtx, dist, debug=debug)
+        plate_elements = find_plate_elements(frame, contours, w, h)
+        plate, plate_r, plate_t = process_plate(plate_elements, frame, original, mtx, dist, debug=debug)
 
-    camera_to_plate = np.concatenate([
-        np.concatenate([np.array(cv.transpose(cv.Rodrigues(plate_r)[0])),
-                        np.array(- cv.transpose(cv.Rodrigues(plate_r)[0]) @ plate_t)], axis=1),
-        np.array([[0, 0, 0, 1]])
-    ], axis=0)
+        camera_to_plate = np.concatenate([
+            np.concatenate([np.array(cv.transpose(cv.Rodrigues(plate_r)[0])),
+                            np.array(- cv.transpose(cv.Rodrigues(plate_r)[0]) @ plate_t)], axis=1),
+            np.array([[0, 0, 0, 1]])
+        ], axis=0)
 
-    center = cv.projectPoints(np.array([
-        [0, 0, 0],
-    ], dtype=np.float32), plate_r, plate_t, mtx, dist)[0][0][0]
+        center = cv.projectPoints(np.array([
+            [0, 0, 0],
+        ], dtype=np.float32), plate_r, plate_t, mtx, dist)[0][0][0]
 
-    third = find_laser_plate_point(original.copy(), center)
-    # cv.drawMarker(frame, third, (255, 0, 0), cv.MARKER_TRIANGLE_UP, 15, 3)
+        third = find_laser_plate_point(original.copy(), center, debug=debug)
+        cv.drawMarker(frame, third, (255, 0, 0), cv.MARKER_CROSS, 10, 1)
 
-    third_camera = np.array([third[0] - (w // 2), third[1] - (h // 2), mtx[0][0], 1])
+        third_camera = np.array([third[0] - (w // 2), third[1] - (h // 2), mtx[0][0], 1])
 
-    third_plate = camera_to_plate @ third_camera
-    third_plate = [third_plate[0] / third_plate[3], third_plate[1] / third_plate[3],
-                   third_plate[2] / third_plate[3]]
+        third_plate = camera_to_plate @ third_camera
+        third_plate = [third_plate[0] / third_plate[3], third_plate[1] / third_plate[3],
+                       third_plate[2] / third_plate[3]]
 
-    origin = - np.transpose(cv.Rodrigues(plate_r)[0]) @ np.array(plate_t)
-    origin = origin.transpose()[0]
+        origin = - np.transpose(cv.Rodrigues(plate_r)[0]) @ np.array(plate_t)
+        origin = origin.transpose()[0]
 
-    direction_vector = third_plate - origin
-    t = - origin[2] / direction_vector[2]
-    intersection_point = origin + (t * direction_vector)
+        direction_vector = third_plate - origin
+        t = - origin[2] / direction_vector[2]
+        intersection_point = origin + (t * direction_vector)
 
-    back_to_camera = np.concatenate([
-        np.concatenate([cv.Rodrigues(back_r)[0], back_t], axis=1),
-        np.array([[0, 0, 0, 1]])
-    ], axis=0)
-    first_back = [((-back_c - back_b * 0) / back_a), 0, 0, 1]
-    second_back = [((-back_c - back_b * 230) / back_a), 230, 0, 1]
-    first_camera = back_to_camera @ first_back
-    second_camera = back_to_camera @ second_back
-    first_plate = camera_to_plate @ first_camera
-    first_plate = [first_plate[0] / first_plate[3], first_plate[1] / first_plate[3], first_plate[2] / first_plate[3]]
-    second_plate = camera_to_plate @ second_camera
-    second_plate = [second_plate[0] / second_plate[3], second_plate[1] / second_plate[3],
-                    second_plate[2] / second_plate[3]]
+        back_to_camera = np.concatenate([
+            np.concatenate([cv.Rodrigues(back_r)[0], back_t], axis=1),
+            np.array([[0, 0, 0, 1]])
+        ], axis=0)
+        first_back = [((-back_c - back_b * 0) / back_a), 0, 0, 1]
+        second_back = [((-back_c - back_b * 230) / back_a), 230, 0, 1]
+        first_camera = back_to_camera @ first_back
+        second_camera = back_to_camera @ second_back
+        first_plate = camera_to_plate @ first_camera
+        first_plate = [first_plate[0] / first_plate[3], first_plate[1] / first_plate[3],
+                       first_plate[2] / first_plate[3]]
+        second_plate = camera_to_plate @ second_camera
+        second_plate = [second_plate[0] / second_plate[3], second_plate[1] / second_plate[3],
+                        second_plate[2] / second_plate[3]]
 
-    plane = find_plane_equation(np.array(second_plate), np.array(first_plate), np.array(intersection_point))
+        plane = find_plane_equation(np.array(second_plate), np.array(first_plate), np.array(intersection_point))
 
-    o, _ = cv.projectPoints(np.array([
-        intersection_point,
-        [0, 0, 0],
-        first_plate,
-        second_plate
-    ], dtype=np.float32), plate_r, plate_t, mtx, dist)
-
-    cv.line(frame, [round(i) for i in o[0][0]], [round(i) for i in o[1][0]], (0, 0, 0), 5)
-    cv.drawMarker(frame, [round(i) for i in o[0][0]], (0, 255, 0), cv.MARKER_CROSS, 30, 5)
-    cv.drawMarker(frame, [round(i) for i in o[2][0]], (255, 255, 0), cv.MARKER_CROSS, 30, 5)
-    cv.drawMarker(frame, [round(i) for i in o[3][0]], (255, 0, 0), cv.MARKER_CROSS, 30, 5)
-
-    laser_points = detect_laser_points(original.copy(), plate)
-    for i in laser_points:
-        i_camera = np.array([i[0] - (w / 2), i[1] - (h / 2), mtx[0][0], 1])
-        i_plate = camera_to_plate @ i_camera
-        i_plate = [i_plate[0] / i_plate[3], i_plate[1] / i_plate[3],
-                   i_plate[2] / i_plate[3]]
-
-        point = find_plane_line_intersection(plane, origin, i_plate)
-
-        test, _ = cv.projectPoints(np.array([
-            point,
+        o, _ = cv.projectPoints(np.array([
+            intersection_point,
+            [0, 0, 0],
+            first_plate,
+            second_plate
         ], dtype=np.float32), plate_r, plate_t, mtx, dist)
 
-        output_file.write(f"{point[0]} {point[1]} {point[2]}\n")
-        cv.drawMarker(frame, [round(m) for m in test[0][0]], (0, 0, 255), cv.MARKER_TILTED_CROSS, 5, 1)
-        # cv.drawMarker(frame, [round(m) for m in i], (0, 255, 0), cv.MARKER_CROSS, 5, 1)
+        cv.line(frame, [round(i) for i in o[0][0]], [round(i) for i in o[1][0]], (0, 0, 0), 5)
+        cv.drawMarker(frame, [round(i) for i in o[0][0]], (0, 255, 0), cv.MARKER_CROSS, 30, 5)
+        cv.drawMarker(frame, [round(i) for i in o[2][0]], (255, 255, 0), cv.MARKER_CROSS, 30, 5)
+        cv.drawMarker(frame, [round(i) for i in o[3][0]], (255, 0, 0), cv.MARKER_CROSS, 30, 5)
+
+        laser_points = detect_laser_points(original.copy(), plate, debug=debug)
+        for i in laser_points:
+            i_camera = np.array([i[0] - (w / 2), i[1] - (h / 2), mtx[0][0], 1])
+            i_plate = camera_to_plate @ i_camera
+            i_plate = [i_plate[0] / i_plate[3], i_plate[1] / i_plate[3],
+                       i_plate[2] / i_plate[3]]
+
+            point = find_plane_line_intersection(plane, origin, i_plate)
+
+            test, _ = cv.projectPoints(np.array([
+                point,
+            ], dtype=np.float32), plate_r, plate_t, mtx, dist)
+
+            output_file.write(f"{point[0]} {point[1]} {point[2]}\n")
+            cv.drawMarker(frame, [round(m) for m in test[0][0]], (0, 0, 255), cv.MARKER_TILTED_CROSS, 5, 1)
+            # cv.drawMarker(frame, [round(m) for m in i], (0, 255, 0), cv.MARKER_CROSS, 5, 1)
+
+    except Exception as error:
+        print(colored(f"ERROR: {error}", "red"))
 
     cv.putText(frame,
                f"{str(round(int(cap.get(cv.CAP_PROP_POS_FRAMES)) / int(cap.get(cv.CAP_PROP_FRAME_COUNT)) * 100, 2))}%",
